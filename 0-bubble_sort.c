6ceb0ec035ddaeaded49e5b4aa290ef7aba41212
mabindajoash <mabindajoash@gmail.com>
2024-10-28 04:32:41 -0700

    selection sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+  * bubble_sort - sorts arrays of integers inascending order using bubble sort algorithm
+  * @array: array to be sorted
+  * @size: size of the array
+  * Return: No return value
+  */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t temp, i, j;
+	size_t n = size;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = 0; j < n - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+			else
+				continue;
+			print_array(array, n);
+		}
+	}
+	return;
+}
---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+  * selection_sort - sort an array of integers using selection sort
+  * @array: array to be sorted
+  * @size: size of the array
+  * Return: no return value
+  */
+void selection_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, j, min_idx;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min_idx] > array[j])
+				min_idx = j;
+		}
+		if (min_idx != i)
+		{
+			temp = array[min_idx];
+			array[min_idx] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
+	}
+	return;
+}
