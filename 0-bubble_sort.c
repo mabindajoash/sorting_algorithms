93d09956a94619506d1a73b2c3af058b7151e262
mabindajoash <mabindajoash@gmail.com>
2024-10-28 07:33:21 -0700

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,13 +12,16 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t temp, i, j;
 	size_t n = size;
+	int swapped;
 
 	for (i = 0; i < n - 1; i++)
 	{
+		swapped = 0;
 		for (j = 0; j < n - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
+				swapped = 1;
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
@@ -27,5 +30,7 @@ void bubble_sort(int *array, size_t size)
 				continue;
 			print_array(array, n);
 		}
+		if (swapped == 0)
+			break;
 	}
 }
