af82921275a72697b809015b456267719cb1a160
mabindajoash <mabindajoash@gmail.com>
2024-11-01 04:34:53 -0700

    radix

--- sort.h
+++ sort.h
@@ -34,8 +34,16 @@ int partition(int *arr, int first, int last, size_t size);
 void quick_sort(int *array, size_t size);
 void quickSort(int *arr, int first, int last, size_t size);
 void swap(int *a, int *b);
-
-
+void shell_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void radix_sort(int *array, size_t size);
 
 
 
